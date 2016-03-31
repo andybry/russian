d014ce75865fc6bda7a17d64fefa82093d30d7b5
Andrew Bryant <ar.bryant@btinternet.com>
2016-03-31 19:02:42 +0000

    Add basic webpack dev configuration

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/client',
+  output: {
+    filename: 'bundle.js',
+    path: './public',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin
+  ]
+}
