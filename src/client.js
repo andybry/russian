81e73f29f2b69f785c6cf63879ead492b14bf2ee
Andrew Bryant <ar.bryant@btinternet.com>
2016-03-31 21:29:36 +0000

    Load the data files

--- src/client.js
+++ src/client.js
@@ -1,1 +1,7 @@
+const lemmas = require('../data/lemma.num.utf-8')
+const wordForms = require('../data/words.num.utf-8')
+
+console.log("lemma length: ", lemmas.length)
+console.log("word forms length: ", wordForms.length)
+
 document.write('prb')
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin
-  ]
+  ],
+  module: {
+    loaders: [
+      { test: /\.utf-8$/, loader: 'raw' }
+    ]
+  }
 }
