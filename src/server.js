7c16be45c542329ecc789ac361c6c657c0af5016
Andrew Bryant <ar.bryant@btinternet.com>
2016-04-04 01:06:51 +0000

    Linting error

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const root = path.resolve(__dirname + '/../public')
+const root = path.resolve(`${__dirname}/../public`)
 app.use(express.static(root))
-app.use((req, res) => res.sendFile(root + '/index.html'))
+app.use((req, res) => res.sendFile(`${root}/index.html`))
+// eslint-disable-next-line no-console
 app.listen(process.env.PORT, () => console.log('server started'))
